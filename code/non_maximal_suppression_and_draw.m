225d6bd4e40c351d1e6a2f7f294d083caa232c60
Mohamed Soliman <mohamed.soliman@aalto.fi>
2016-01-13 13:57:40 +0200

    changed location of edited non maximal suppression frames

--- code/non_maximal_suppression_and_draw.m
+++ code/non_maximal_suppression_and_draw.m
@@ -1,10 +1,7 @@
 %code for performing non maximal suppression on upper body data and drawing
 %on images to check
 
-%shape = rectangles 
-%BorderColor = custom
-%CustomBorderColor = uint8([255 0 0])
-%lineWidth = 5
+
 selectedBboxes = {};
 for i=9402:9757
     disp(size(processedupper(processedupper(:,1)==i,2:5)))
@@ -16,27 +13,16 @@ shapeInserter = vision.ShapeInserter('Shape','Rectangles','BorderColor','Custom'
 
 for i=1:353
     index = sprintf('%04d',i); 
-   % disp(index);
     path = strcat('/home/osman/Dropbox/HCID Master/2nd Year - Aalto/Research Project/data/subdata/new_data_app/frames/frame_',index,'.jpg');
     disp(path);
     I = imread(path);
-   % x = 300;
-   % y = 250;
-   % w = 550;
-   % h = 550;
-    % [x y width height].
-   % for j=1:size(detections(i+1))
-    
+ 
     w = selectedBboxes{i}(:,3);
     h = selectedBboxes{i}(:,4);
     or_x = selectedBboxes{i}(:,1) - w/2;
     or_y = selectedBboxes{i}(:,2) - h/2;
-   % or_x = detections(i+1).x(:) - (detections(i+1).w(:)/2);
-    %or_y = detections(i+1).y(:) - (detections(i+1).h(:)/2);
-    %rectangle = int32([or_x or_y detections(i+1).w(:) detections(i+1).h(:)]);
     rectangle = int32([or_x or_y w h]);
 
     J = step(shapeInserter, I, rectangle);
-    %end
     imwrite(J,strcat('/home/osman/Dropbox/HCID Master/2nd Year - Aalto/Research Project/data/subdata/new_data_app/frames/sedit_image_',index,'.jpg'));
 end
